364d1f3d7d14402d180020cacdab50ad8e29276a
mdaogaru <mdaogaru@gmail.com>
2016-12-05 09:40:25 +0000

    start of the CW

--- CW 2/main_CW2.py
+++ CW 2/main_CW2.py
@@ -1,7 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 24 13:22:15 2016
 
-@author: md2g12
-"""
+
+import numpy
+
+
+def P(t, dy, alpha, beta):
+    return alpha * dy**2 + beta * (t**2 - 1) * dy**3
+
+
+def L(t, dy, y, alpha, beta):
+    return P(t, dy, alpha, beta) - y
 
