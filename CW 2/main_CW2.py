cf5f5adf09323fce4550ede91972800f15c510d0
mdaogaru <mdaogaru@gmail.com>
2016-12-05 18:42:22 +0000

    created the dqdt function

--- CW 2/main_CW2.py
+++ CW 2/main_CW2.py
@@ -3,6 +3,12 @@ from matplotlib import pyplot
 from scipy.integrate import odeint
 from scipy.optimize import newton
 
+
+y_A = 1.
+y_B = 0.9
+
+
+
 def P(t, dy, alpha, beta):
     return alpha * dy**2 + beta * (t**2 - 1) * dy**3
 
@@ -11,9 +17,9 @@ def L(t, y, dy, alpha, beta):
     return P(t, dy, alpha, beta) - y
 
 
-def dLdy(L, t, q, h, alpha, beta):
+def dL_dy(L, t, q, h, alpha, beta):
     """central differencing on dt"""
-    return (L(t, q[0]+h, q[1], alpha, beta) - L(t, q[0-h, q[1], alpha, beta)) / (2*h)
+    return (L(t, q[0]+h, q[1], alpha, beta) - L(t, q[0]-h, q[1], alpha, beta)) / (2*h)
 
 
 def d2L_dtdydot(L, t, q, h, alpha, beta):
@@ -29,30 +35,42 @@ def d2L_dydydot(L, t, q, h, alpha, beta):
                    L(t, q[0]-h, q[1]-h, alpha, beta)) / (4*h**2)
 
 
-def d2L_dydot2(l, t, q, h, alpha, beta):
+def d2L_dydot2(L, t, q, h, alpha, beta):
         return (L(t, q[0], q[1]+h, alpha, beta) - \
                 2 * L(t, q[0], [1], alpha, beta) + \
                 L(t, q[0], q[1]-h, alpha, beta)) / h**2
 
-
-
- def f(q, x):
-    dqdx = numpy.zeros_like(q)
+def f(L, t, q, h, alpha, beta):
+    """dqdx = numpy.zeros_like(q)
     dqdx[0] = q[1]
     dqdx[1] = -1 - q[1]
-    return dqdx
+    return dqdx"""
+    return (dL_dy(L, t, q, h, alpha, beta) - \
+             d2L_dtdydot(L, t, q, h, alpha, beta) - \
+             d2L_dydydot(L, t, q, h, alpha, beta)) / \
+             d2L_dydot2(L, t, q, h, alpha, beta)
+
+def dq_dt(L, t, q, h, alpha, beta):
+    dqdt = numpy.zeros_like(q)
+    dqdt[0] = q[1]
+    dqdt[1] = f(L, t, q, h, alpha, beta)
+    return dqdt
+
+
+def phi_z(t, q, z):
+    return q[0] - y_B
 
 def shooting_ivp(z):
-    soln = odeint(f, [y0b, z], [0, 1])
+    soln = odeint(f, [y_A, z], [0, 1])
     y_boundary = soln[-1, 0]
-    return y_boundary - 0.9
+    return y_boundary - y_B
 
 def shooting():
     z_guess = 0.1
     z_proper = newton(shooting_ivp, z_guess)
-    x = numpy.linspace(0, 1)
-    soln = odeint(f, [0, z_proper], x)
-    return x, soln[:, 0]
+    t = numpy.linspace(0, 1)
+    soln = odeint(f, [0, z_proper], t)
+    return t, soln[:, 0]
 
 
 
