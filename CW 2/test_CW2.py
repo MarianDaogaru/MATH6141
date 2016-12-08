f7607d66bf4a48828967800e35b504b99d8c3bd5
mdaogaru <mdaogaru@gmail.com>
2016-12-08 14:41:17 +0000

    added several tests for up until dtdydot

--- CW 2/test_CW2.py
+++ CW 2/test_CW2.py
@@ -19,6 +19,63 @@ def test_P():
         assert numpy.allclose(val, val_P), \
             "P does not create proper values."
 
+def test_L():
+    for i in range(10):
+        t = numpy.random.random()
+        dy = numpy.random.random()
+        a = numpy.random.random()
+        b = numpy.random.random()
+        y = numpy.random.random()
+        val = a * dy * dy + b * (t * t - 1) * dy * dy * dy - y
+        val_L = L(t, y, dy, a, b)
+        assert type(val_L) == float, \
+            "L does not create proper type values."
+        assert numpy.allclose(val, val_L), \
+            "L does not create proper values."
+
+def test_dL_dy():
+    for i in range(10):
+        t = numpy.random.random()
+        h = numpy.random.random()
+        a = numpy.random.random()
+        b = numpy.random.random()
+        q = numpy.random.random(2)
+        dy = q[1]
+        y = q[0]
+        val = (a * dy * dy + b * (t * t - 1) * dy * dy * dy - (y+h) - \
+            (a * dy * dy + b * (t * t - 1) * dy * dy * dy - (y-h))) / (2*h)
+        val_dl = dL_dy(L, t, q, h, a, b)
+        assert type(val_dl) == float or type(val_dl) == numpy.float64, \
+            "dL_dy didn't create proper type"
+        assert numpy.allclose(val, val_dl), \
+            "values don't match"
+
+def test_d2L_dtdydot():
+    for i in range(10):
+        t = numpy.random.random()
+        h = numpy.random.random()
+        a = numpy.random.random()
+        b = numpy.random.random()
+        q = numpy.random.random(2)
+        dy = q[1]
+        y = q[0]
+        val_dl = d2L_dtdydot(L, t, q, h, a, b)
+        t1 = t + h
+        t2 = t - h
+        dy1 = q[1]+h
+        dy2 = q[1]-h
+        k1 = a * dy1 * dy1 + b * (t1 * t1 - 1) * dy1 * dy1 * dy1 - y
+        k2 = a * dy2 * dy2 + b * (t1 * t1 - 1) * dy2 * dy2 * dy2 - y
+        k3 = a * dy1 * dy1 + b * (t2 * t2 - 1) * dy1 * dy1 * dy1 - y
+        k4 = a * dy2 * dy2 + b * (t2 * t2 - 1) * dy2 * dy2 * dy2 - y
+
+        val = (k1 - k2 - k3 + k4) / (4 * h**2)
+
+        assert type(val_dl) == float or type(val_dl) == numpy.float64, \
+            "dL_dy didn't create proper type"
+        assert numpy.allclose(val, val_dl), \
+            "values don't match"
+
 
 if __name__ == "__main__":
     pytest.main()
